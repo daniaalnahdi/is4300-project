574334858a92221bc27d09f7fcab03fb0589e8fb
daniaalnahdi <alnahdi.d@husky.neu.edu>
2021-03-27 16:16:40 +0300

    add demo spinner

--- src/views/RaisedHandsView.jsx
+++ src/views/RaisedHandsView.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import Loader from '../assets/loader.gif'
 import students from '../data/students';
 
 const RaisedHandsView = () => {
+    const [show, setShow] = useState(false);
+    
   return (
     <div>
       <h2>Raised Hands View</h2>
-      <p>Raised Recently:</p>
+      <img src={Loader} style={{width: '50px'}}/>
+      <span>Finding new raised hands:</span>
       <ul>
         {students.map((name, idx) => {
           return idx <= 2 && <li key={idx}>{name}</li>;
         })}
       </ul>
-      <p>Other:</p>
+      <p>Raised previously:</p>
       <ul>
         {students.map((name, idx) => {
           return idx > 2 && <li key={idx}>{name}</li>;
         })}
       </ul>
+      <Link to='/pick-student'>
+        <button>Pick Random Student</button>
+      </Link>
       <Link to='/'>
-        <Link to='/pick-student'>
-          <button>Pick Random Student</button>
-        </Link>
         <button>Done</button>
       </Link>
     </div>
